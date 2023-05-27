d8bd1ea3c82f6b46a8bdf67efb63c8026eac9c84
Eric L. Hernes <eric@qinc.tv>
2023-05-27 09:16:48 -0500

    add tests for custom types; add asserts for stack size

--- stack-test.cpp
+++ stack-test.cpp
@@ -14,23 +14,76 @@
 
 #include "rpn.h"
 
+class CustomArray : public rpn::Stack::Object {
+public:
+  CustomArray() = default;
+  CustomArray(const CustomArray &a)  {
+    for(auto const &e : a._v) {
+      _v.push_back(e->deep_copy());
+    }
+  }
+  virtual std::unique_ptr<rpn::Stack::Object> deep_copy() const { return std::make_unique<CustomArray>(*this); };
+  void add_value(const rpn::Stack::Object &val) {
+    _v.push_back(val.deep_copy());
+  }
+  virtual operator std::string() const {
+    std::string rv = "[";
+    for(auto const &e : _v) {
+      rv += e->to_string();
+      rv += ", ";
+    }
+    rv += "]";
+    return rv;
+  };
+  auto const &val() const { return _v; };
+private:
+  std::vector<std::unique_ptr<rpn::Stack::Object>> _v;
+};
+
+class CustomObject {
+ public:
+  CustomObject(const std::string &v) : _v(v) {};
+  virtual operator std::string() const { return std::string("CustomObject: <") + _v + ">"; };
+  auto val() const { return _v; }
+private:
+  std::string _v;
+};
+
+using StCustomObject = TStackObject<CustomObject>;
+
 int
 main(int ac, char **av) {
+  int stackCount = 0;
   rpn::Stack stack;
   //  stack.push(StDouble(3.1415));
-  stack.push_double(3.1415);
-  stack.push_string("wxyz");
+  stack.push_double(3.1415); stackCount++;
+  assert (stack.depth() == stackCount);
+  stack.push_string("wxyz"); stackCount++;
+  assert (stack.depth() == stackCount);
   //  stack.push(StInteger(1234));
-  stack.push_integer(1234);
-  stack.push(StString(std::string("abcdefg")));
-  stack.push(StDouble(19.2));
+  stack.push_integer(1234); stackCount++;
+  assert (stack.depth() == stackCount);
+  {
+    CustomArray ar1;
+    ar1.add_value(StDouble(9.8));
+    ar1.add_value(StString(std::string("yyz")));
+    stack.push(ar1); stackCount++;
+    assert (stack.depth() == stackCount);
+  }
+		 
+  stack.push(StString(std::string("abcdefg"))); stackCount++;
+  assert (stack.depth() == stackCount);
 
-  assert (stack.depth() == 5);
+  stack.push(StDouble(19.2)); stackCount++;
+  assert (stack.depth() == stackCount);
+
+  assert (stack.depth() == stackCount);
 
   {
-    double v = stack.pop_double(); // std::unique_ptr<StDouble> v(static_cast<StDouble*>(stack.pop().release()));
+    double v = stack.pop_double(); stackCount--;// std::unique_ptr<StDouble> v(static_cast<StDouble*>(stack.pop().release()));
     printf("popped double (%f)\n", v);
-  }
+    assert (stack.depth() == stackCount);
+}
 
   {
     const StString &tsr(dynamic_cast<const StString&>(stack.peek(1)));
@@ -38,7 +91,10 @@ main(int ac, char **av) {
     stack.print();
   }
 
-  stack.push_boolean(true);
+  stack.push(StCustomObject(std::string("yyz"))); stackCount++;
+  assert (stack.depth() == stackCount);
+  
+  stack.push_boolean(true); stackCount++;
 
   stack.swap();
   stack.print("swap");
@@ -46,45 +102,57 @@ main(int ac, char **av) {
   stack.rolldn(2);
   stack.print("rolld(2) [aka swap]");
   
-  stack.pick(2);
+  stack.pick(2); stackCount++;
   stack.print("over");
 
-  stack.pick(4);
+  stack.pick(4); stackCount++;
   stack.print("pick(4)");
 
-  stack.push(StDouble(2.7));
-  stack.dup();
+  stack.push(StDouble(2.7)); stackCount++;
+  stack.dup(); stackCount++;
+  assert (stack.depth() == stackCount);
   stack.print("push(2.7) dup");
 
   //  stack.push(StString("marker"));
   int n = stack.depth();
-  stack.dupn(n);
+  stack.dupn(n);  stackCount+=n;
+  assert (stack.depth() == stackCount);
   stack.print("depth dupn");
 
-  stack.dropn(n);
+  stack.dropn(n); stackCount-=n;
+  assert (stack.depth() == stackCount);
   stack.print("dropn");
 
-  stack.dupn(n);
+  stack.dupn(n); stackCount+=n;
+  assert (stack.depth() == stackCount);
   stack.print("dupn");
 
   stack.rollun(n/2);
+  assert (stack.depth() == stackCount);
   printf("rollun(%d)\n", n/2);
   stack.print("rollun");
+  assert (stack.depth() == stackCount);
 
   stack.rollu();
+  assert (stack.depth() == stackCount);
   stack.rollu();
+  assert (stack.depth() == stackCount);
   stack.print("rollu rollu");
 
   stack.rolld();
+  assert (stack.depth() == stackCount);
   stack.print("rolld");
 
   stack.rolldn(4);
+  assert (stack.depth() == stackCount);
   stack.print("rolld(4)");
 
-  stack.tuckn(5);
+  stack.tuckn(5); stackCount++;
+  assert (stack.depth() == stackCount);
   stack.print("tuckn(5)");
 
-  stack.nipn(7);
+  stack.nipn(7); stackCount--;
+  assert (stack.depth() == stackCount);
   stack.print("nipn(7)");
 
   StObject obj;
